3efdc4738c78e20c47fa7f333a7edccf6f796a8c
Paige Dionne <134656374+kagexin@users.noreply.github.com>
2023-07-23 21:17:25 -0400

    Remove button functionality for future update

--- javascript.js
+++ javascript.js
@@ -15,24 +15,6 @@ function divide(param1, param2) {
 }
 
 
-const displayText = document.querySelector(".display-text");
-
-function addButtonFunctionality(className, textContent) {
-    const button = document.querySelector(className);
-    button.addEventListener("click", () => changeDisplayText(textContent))
+function operate(param1, operand, param2) {
+    
 }
-
-function changeDisplayText(text) {
-    displayText.textContent += text;
-}
-
-addButtonFunctionality(".zero", "0")
-addButtonFunctionality(".one", "1");
-addButtonFunctionality(".two", "2");
-addButtonFunctionality(".three", "3");
-addButtonFunctionality(".four", "4");
-addButtonFunctionality(".five", "5");
-addButtonFunctionality(".six", "6");
-addButtonFunctionality(".seven", "7");
-addButtonFunctionality(".eight", "8");
-addButtonFunctionality(".nine", "9");
