43513284d409778f343dec7a49b25d3ef225a4e0
Paige Dionne <134656374+kagexin@users.noreply.github.com>
2023-07-25 18:11:25 -0400

    Add functionality for secondary display

--- javascript.js
+++ javascript.js
@@ -1,39 +1,88 @@
-let result;
-let prevResult;
+// Operation functions
 
-
-function add(num1, num2) {
-    result = (num1 + num2)
-    return result;
+function addition(num1, num2) {
+    return (num1 + num2)
 }
 
-function subtract(num1, num2) {
-    result = (num1 - num2);
-    return result;
+function subtraction(num1, num2) {
+    return (num1 - num2);
 }
 
-function multiply(num1, num2) {
-    result = (num1 - num2);
-    return result;
+function multiplication(num1, num2) {
+    return (num1 * num2);
 }
 
-function divide(num1, num2) {
-    result = (num1 - num2);
-    return result;
+function division(num1, num2) {
+    return (num1 / num2);
 }
 
 
-function operate(param1, operand, param2) {
-    if (operand = "+" ) {
-        add(param1, param2);
-        prevResult = result;
-    }
+// Function to operate between two numbers
+
+function operate(param1, param2, operand) {
+     switch (operand) {
+        case "+":
+            return addition(param1, param2);
+        case "-":
+            return subtraction(param1, param2);
+        case "÷":
+            return division(param1, param2);
+        case "×":
+            return multiplication(param1, param2);
+     }  
 }
+
+
+// Make number button presses show up in calculator display
 
 const numButton = document.querySelectorAll(".numButton");
 const displayText = document.querySelector(".display-text");
 
-numButton.forEach(element => element.addEventListener("click", () => {
-    displayText.textContent += element.textContent;
+numButton.forEach(button => button.addEventListener("click", () => {
+    displayText.textContent += button.textContent;
     })
 )
+
+
+// Make clear and delete buttons functional
+
+const clear = document.querySelector("#clear");
+const del = document.querySelector("#delete");
+const prevResultDisplay = document.querySelector(".prev-result");
+
+clear.addEventListener("click", () => {
+    displayText.textContent = "";
+    prevResultDisplay.textContent = "";
+})
+
+del.addEventListener("click", () => {
+    displayText.textContent = displayText.textContent.slice(0, -1);
+})
+
+
+// Make operand and equals buttons functional
+
+const equals = document.querySelector("#equals");
+const operandBtns = document.querySelectorAll(".operand");
+let operand, num1, num2, result;
+
+operandBtns.forEach(button => button.addEventListener("click", (event) => {
+    num1 = parseInt(displayText.textContent);
+    operand = event.target.textContent;
+    prevResultDisplay.textContent = `${num1} ${operand}`;
+    displayText.textContent = "";
+    })
+)
+
+equals.addEventListener("click", () => equate())
+
+
+function equate() {
+    num2 = parseInt(displayText.textContent);
+    prevResultDisplay.textContent = `${num1} ${operand} ${num2} =`
+    result = operate(num1, num2, operand);
+
+    if (displayText.textContent != "") {
+        displayText.textContent = result;
+    }
+}
